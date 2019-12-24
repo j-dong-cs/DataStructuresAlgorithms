1e76ad618f77ae2d68f0d69c035aa230a259e78f
j-dong-cs <jingjing.dong.mil@outlook.com>
2019-12-23 22:33:55 -0800

    Initial check-in of module DataStructuresAlgorithms

---
+++ DataStructuresAlgorithms/BinaryTreeIterativeTraversal.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructuresAlgorithms
+{
+    public class BinaryTreeIterativeTraversal
+    {
+        // This method iteratively traverse the binray tree in preorder.
+        public static List<int> PreorderIterativeTraverse(TreeNode root)
+        {
+            List<int> preorder = new List<int>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            if (root != null)
+            {
+                stack.Push(root);
+            }
+
+            while (stack.Count > 0)
+            {
+                // Pop TreeNode in the stack and add to the preorder list
+                TreeNode node = stack.Pop();
+                preorder.Add(node.val);
+
+                // Push node's children into stack; right first then left
+                if (node.right != null) stack.Push(node.right);
+                if (node.left != null) stack.Push(node.left);
+            }
+
+            return preorder;
+        }
+
+        // This method iteratively traverse the binray tree in inorder.
+        public static List<int> InorderIterativeTraverse(TreeNode root)
+        {
+            List<int> inorder = new List<int>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            // Push all the left children into stack
+            TreeNode cur = root;
+            while (stack.Count > 0 || cur != null)
+            {
+                // push all the left children
+                while (cur != null)
+                {
+                    stack.Push(cur);
+                    cur = cur.left;
+                }
+
+                // pop root
+                cur = stack.Pop();
+                inorder.Add(cur.val);
+                // go to right child
+                cur = cur.right;
+            }
+
+            return inorder;
+        }
+
+        // This method iteratively traverse the binray tree in postorder.
+        public static List<int> PostorderIterativeTraverse(TreeNode root)
+        {
+            List<int> postorder = new List<int>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            TreeNode cur = root;
+            TreeNode lastNodeVisited = null;
+            while (stack.Count > 0 || cur != null)
+            {
+                while (cur != null)
+                {
+                    stack.Push(cur);
+                    cur = cur.left;
+                }
+
+                TreeNode peekNode = stack.Peek();
+                if (peekNode.right != null && lastNodeVisited != peekNode.right)
+                {
+                    cur = peekNode.right;
+                }
+                else
+                {
+                    postorder.Add(peekNode.val);
+                    lastNodeVisited = stack.Pop();
+                }
+
+            }
+            return postorder;
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val)
+            {
+                this.val = val;
+                this.left = null;
+                this.right = null;
+            }
+        }
+    }
+
+}
---
+++ Tests/BinaryTreeIterativeTraversalTests.cs
@@ -0,0 +1,117 @@
+using System.Collections.Generic;
+using Xunit;
+using DataStructuresAlgorithms;
+
+namespace Tests
+{
+    public class BinaryTreeIterativeTraversalTests
+    {
+        BinaryTreeIterativeTraversal.TreeNode root;
+        List<int> expectedList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        [Fact]
+        public void PreorderIterativeTest()
+        {
+            // Test Empty tree
+            root = null;
+            Assert.Equal<List<int>>(new List<int>(), BinaryTreeIterativeTraversal.PreorderIterativeTraverse(root));
+
+            // 1. Arrange
+            // Build up a binary tree
+            //           1
+            //        /     \
+            //       2       6
+            //      /  \    /  \
+            //     3    5  7    10
+            //    /       /
+            //   4       8
+            //            \
+            //             9
+            root = new BinaryTreeIterativeTraversal.TreeNode(1);
+            root.left = new BinaryTreeIterativeTraversal.TreeNode(2);
+            root.right = new BinaryTreeIterativeTraversal.TreeNode(6);
+            root.left.left = new BinaryTreeIterativeTraversal.TreeNode(3);
+            root.left.right = new BinaryTreeIterativeTraversal.TreeNode(5);
+            root.right.left = new BinaryTreeIterativeTraversal.TreeNode(7);
+            root.right.right = new BinaryTreeIterativeTraversal.TreeNode(10);
+            root.right.left.left = new BinaryTreeIterativeTraversal.TreeNode(8);
+            root.right.left.left.right = new BinaryTreeIterativeTraversal.TreeNode(9);
+            root.left.left.left = new BinaryTreeIterativeTraversal.TreeNode(4);
+
+            // 2. Assert
+            List<int> preorder = BinaryTreeIterativeTraversal.PreorderIterativeTraverse(root);
+            for (int i = 0; i < preorder.Count; i++)
+            {
+                Assert.Equal(expectedList[i], preorder[i]);
+            }
+        }
+
+        [Fact]
+        public void InorderIterativeTest()
+        {
+            // Test Empty tree
+            root = null;
+            Assert.Equal<List<int>>(new List<int>(), BinaryTreeIterativeTraversal.InorderIterativeTraverse(root));
+
+            //           5
+            //        /     \
+            //       3       9
+            //      /  \    /  \
+            //     2    4  8    10
+            //    /       /
+            //   1       6
+            //            \
+            //             7
+            root = new BinaryTreeIterativeTraversal.TreeNode(5);
+            root.left = new BinaryTreeIterativeTraversal.TreeNode(3);
+            root.right = new BinaryTreeIterativeTraversal.TreeNode(9);
+            root.left.left = new BinaryTreeIterativeTraversal.TreeNode(2);
+            root.left.right = new BinaryTreeIterativeTraversal.TreeNode(4);
+            root.right.left = new BinaryTreeIterativeTraversal.TreeNode(8);
+            root.right.right = new BinaryTreeIterativeTraversal.TreeNode(10);
+            root.left.left.left = new BinaryTreeIterativeTraversal.TreeNode(1);
+            root.right.left.left = new BinaryTreeIterativeTraversal.TreeNode(6);
+            root.right.left.left.right = new BinaryTreeIterativeTraversal.TreeNode(7);
+
+            List<int> inorder = BinaryTreeIterativeTraversal.InorderIterativeTraverse(root);
+            for (int i = 0; i < inorder.Count; i++)
+            {
+                Assert.Equal(expectedList[i], inorder[i]);
+            }
+        }
+
+        [Fact]
+        public void PostorderIterativeTest()
+        {
+            // Test Empty tree
+            root = null;
+            Assert.Equal<List<int>>(new List<int>(), BinaryTreeIterativeTraversal.PostorderIterativeTraverse(root));
+
+            //           10
+            //        /     \
+            //       4       9
+            //      /  \    /  \
+            //     2    3  7    8
+            //    /       /
+            //   1       6
+            //            \
+            //             5
+            root = new BinaryTreeIterativeTraversal.TreeNode(10);
+            root.left = new BinaryTreeIterativeTraversal.TreeNode(4);
+            root.right = new BinaryTreeIterativeTraversal.TreeNode(9);
+            root.left.left = new BinaryTreeIterativeTraversal.TreeNode(2);
+            root.left.right = new BinaryTreeIterativeTraversal.TreeNode(3);
+            root.right.left = new BinaryTreeIterativeTraversal.TreeNode(7);
+            root.right.right = new BinaryTreeIterativeTraversal.TreeNode(8);
+            root.left.left.left = new BinaryTreeIterativeTraversal.TreeNode(1);
+            root.right.left.left = new BinaryTreeIterativeTraversal.TreeNode(6);
+            root.right.left.left.right = new BinaryTreeIterativeTraversal.TreeNode(5);
+
+            List<int> postorder = BinaryTreeIterativeTraversal.PostorderIterativeTraverse(root);
+            for (int i = 0; i < postorder.Count; i++)
+            {
+                Assert.Equal(expectedList[i], postorder[i]);
+            }
+        }
+    }
+}
