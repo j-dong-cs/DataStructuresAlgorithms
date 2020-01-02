8a311a190a6ca3333e3ca8abf148f5dae9c25c09
j-dong-cs <jingjing.dong.mil@outlook.com>
2020-01-01 20:20:37 -0800

    * MergeSort.cs: Create two implementation Bottomup and Topdown of   MergeSort.
    * MergeSortTests.cs:

---
+++ DataStructuresAlgorithms/MergeSort.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace DataStructuresAlgorithms
+{
+    /*
+     * Generic MergeSort class which provides two implementation:
+     * Topdown and Bottomup.
+     * 
+     * O(n * logn)
+     */
+    public static class MergeSort<T> where T : IComparable<T>
+    {
+        public static void BottomUpSort(T[] a)
+        {
+            T[] aux = new T[a.Length];
+            for (int len = 1; len < a.Length; len *= 2)
+            {
+                for (int lo = 0; lo < a.Length - len; lo += len+len)
+                {
+                    int mid = lo + len - 1;
+                    int hi = Math.Min(lo + len + len - 1, a.Length - 1);
+                    Merge(a, aux, lo, mid, hi);
+                }
+            }
+        }
+
+        // This method sort an generic array with mergesort top-down(divide-conquer) implementation.
+        public static void TopDownSort(T[] a)
+        {
+            T[] aux = new T[a.Length];
+            TopDownSort(a, aux, 0, a.Length - 1);
+        }
+
+        private static void TopDownSort(T[] a, T[] aux, int lo, int hi)
+        {
+            if (hi <= lo) return;
+            int mid = lo + (hi - lo) / 2;
+
+            TopDownSort(a, aux, lo, mid);
+            TopDownSort(a, aux, mid + 1, hi);
+
+            // if array is already sorted, copy array and avoid merge
+            if (a[mid].CompareTo(a[mid + 1]) <= 0)
+            {
+                Array.Copy(a, lo, aux, lo, hi - lo + 1);
+            }
+
+            Merge(a, aux, lo, mid, hi);
+        }
+
+        private static void Merge(T[] a, T[] aux, int lo, int mid, int hi)
+        {
+            // copy 'a' into 'aux'
+            for (int k = lo; k <= hi; k++)
+            {
+                aux[k] = a[k];
+            }
+
+            int i = lo;
+            int j = mid + 1;
+
+            for (int k = lo; k<= hi; k++)
+            {
+                if (i > mid)
+                {
+                    a[k] = aux[j++];
+                }
+                else if (j > hi)
+                {
+                    a[k] = aux[i++];
+                }
+                else if (aux[i].CompareTo(aux[j]) <= 0)
+                {
+                    a[k] = aux[i++];
+                }
+                else
+                {
+                    a[k] = aux[j++];
+                }
+            }
+        }
+    }
+}
---
+++ Tests/MergeSortTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using DataStructuresAlgorithms;
+
+namespace Tests
+{
+    public class MergeSortTests
+    {
+        public static readonly int[] expectInt = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        public static readonly char[] expectChar = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n' };
+
+        [Fact]
+        public void IntTopdownTest()
+        {
+            int[] array = { 10, 0, 3, 11, 8, 9, 1, 12, 5, 2, 4, 6, 7 };
+
+            MergeSort<int>.TopDownSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.Equal(array[i], expectInt[i]);
+            }
+        }
+
+        [Fact]
+        public void StringTopdownTest()
+        {
+            char[] array = { 'm', 'n', 'a', 'k', 'c', 'd', 'g', 'j', 'b', 'l', 'f', 'i', 'e', 'h' };
+
+            MergeSort<char>.TopDownSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.Equal(array[i], expectChar[i]);
+            }
+        }
+
+        [Fact]
+        public void IntBottomupTest()
+        {
+            int[] array = { 10, 0, 3, 11, 8, 9, 1, 12, 5, 2, 4, 6, 7 };
+
+            MergeSort<int>.BottomUpSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.Equal(array[i], expectInt[i]);
+            }
+        }
+
+        [Fact]
+        public void StringBottomupTest()
+        {
+            char[] array = { 'm', 'n', 'a', 'k', 'c', 'd', 'g', 'j', 'b', 'l', 'f', 'i', 'e', 'h' };
+
+            MergeSort<char>.BottomUpSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.Equal(array[i], expectChar[i]);
+            }
+        }
+    }
+}
